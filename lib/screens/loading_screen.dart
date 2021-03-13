468283e03608f0f069056fcc337537580d5873c8
lcw729 <lcw729729@gmail.com>
2021-03-13 13:19:09 +0900

    deconde weather Json

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'location.dart';
+import 'dart:convert';
+
+const apiKey = '88b83ade7037c995188184f8fb9c0107';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,16 +11,41 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longtitude;
+
    void getLocation() async {
      Location location = Location();
      await location.getCurrentLocation();
-     print(location.latitude);
-     print(location.longitude);
+     latitude = location.latitude;
+     longtitude = location.longitude;
+
+     getData();
    }
 
   @override
   void initState() {
      getLocation();
+  }
+
+  void getData() async {
+    http.Response response = await http.get('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longtitude&appid=$apiKey');
+
+    if (response.statusCode == 200){
+      String data = response.body;
+      var decodeData = jsonDecode(data);
+
+      double temperature = decodeData['main']['temp'];
+      int condition = decodeData['weather'][0]['id'];
+      String cityName = decodeData['name'];   
+      print(temperature);
+      print(condition);
+      print(cityName);
+    }
+    else{
+      print(response.statusCode);
+    }
+
   }
 
 
